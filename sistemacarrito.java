39a4fbeeb96022de6941ff87a0544b173b2320f3
nicola-19010 <n.pacheco02@ufromail.cl>
2022-03-23 09:49:13 -0300

    2do commit

--- sistemacarrito.java
+++ sistemacarrito.java
@@ -1,3 +1,27 @@
 public class sistemacarrito {
-    
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i <= listaproductos.length; i++){
+            listaproductos[i] = 500 + (150 * (i));
+
+
+
+    }
+
+    // metodo muestra por pantalla cantidad de productos que contiene el carrito
+    //public static String MostrarC_productos (){
+
+    }
+
+    //metodo largo carrito
+    public static int Generarnum_carrito(){
+        int numero_randomc = (int)(Math.random()*20 + 1);
+        return numero_randomc;
+    }
+
+    //metodo numero de productos
+    public static  int Generarnum_productos(){
+        int numero_randomp = (int)(Math.random()*15 );
+        return numero_randomp;
+    }
 }
